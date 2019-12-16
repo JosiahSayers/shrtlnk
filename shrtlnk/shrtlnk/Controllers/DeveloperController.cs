eeb0a1a9b27c33d3e19f788aa616b4d7995f4d82
Josiah Sayers <josiah.sayers15@gmail.com>
2019-12-15 20:51:06 -0500

    adds developer portal index and lays out portal pages

---
+++ shrtlnk/shrtlnk/Controllers/DeveloperController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shrtlnk.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Documentation()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
