3930c18e7aefb5583565788fbfdc07172a382b4c
Josiah Sayers <jsayers@covermymeds.com>
2022-03-11 09:22:55 -0500

    add try catch around db connection stat

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -2,8 +2,16 @@ import { json, LoaderFunction } from "remix";
 import { db } from "~/utils/db.server";
 
 export const loader: LoaderFunction = async () => {
-  const dbConnected = !!(await db.shrtlnk.findFirst());
+  const dbConnected = await isDbConnected();
   const currentTime = new Date().getTime();
   const status = dbConnected && currentTime ? "ok" : "error";
   return json({ dbConnected, currentTime, status });
+};
+
+const isDbConnected = async () => {
+  try {
+    return !!(await db.shrtlnk.findFirst());
+  } catch (e) {
+    return false;
+  }
 };
