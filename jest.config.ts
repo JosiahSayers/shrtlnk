0ac95adce53d5fcde40e45265fb121c63c0af698
Josiah Sayers <josiah.sayers15@gmail.com>
2022-11-20 19:58:32 -0500

    add module name mapper for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,14 @@
-import type {Config} from '@jest/types';
+import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-  testRegex: "\.test(\.server)?\.ts",
+  testRegex: ".test(.server)?.ts",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   preset: "ts-jest",
-  testEnvironment: "node"
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "~/(.*)": "<rootDir>/app/$1",
+  },
 };
 export default config;
