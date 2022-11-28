f767663eaf4658001e82b3bacee95eba7cc06812
Josiah Sayers <josiah.sayers15@gmail.com>
2022-11-28 14:01:38 -0500

    show all api keys in test

--- cypress/e2e/dev-portal/applications/list.cy.ts
+++ cypress/e2e/dev-portal/applications/list.cy.ts
@@ -46,7 +46,7 @@ describe("Application list", () => {
     cy.findByText(
       (content, node) => node?.textContent === "API Key: test-api-key"
     ).should("not.exist");
-    cy.get(".api-key-toggle").first().click();
+    cy.get(".api-key-toggle").click({ multiple: true });
     cy.findByText(
       (content, node) => node?.textContent === "API Key: test-api-key"
     );
