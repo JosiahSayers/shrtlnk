bf4ea14fa37ddbe952e08aa62167f2fe697f49f6
Josiah Sayers <josiah.sayers15@gmail.com>
2023-06-05 21:30:46 -0400

    shrink default admin stats date range

--- app/components/developer/admin/BarChartCard.tsx
+++ app/components/developer/admin/BarChartCard.tsx
@@ -1,4 +1,12 @@
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 type Props = {
   title: string;
@@ -23,13 +31,15 @@ export default function BarChartCard({
     <div className="card text-center mb-3">
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <BarChart width={width} height={height} data={data} id={id}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={xAxisKey} />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey={dataKey} fill="#8884d8" />
-        </BarChart>
+        <ResponsiveContainer height={height}>
+          <BarChart width={width} height={height} data={data} id={id}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey={xAxisKey} />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey={dataKey} fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
